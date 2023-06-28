2b4089e9cd2b98610171fb23b62f63424ae6df57
tadeu-gabriel <tadeu.gabriel.filho@gmail.com>
2023-06-28 23:53:05 +0100

    Add completed challange lvl01 from RPA Hackathon

---
+++ HackathonTest/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace HackathonTest
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
